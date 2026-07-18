07385303149238c5d079a9874f59ee2d4bedf78c
Raamviyhash <raamviyhash811@gmail.com>
2026-07-18 15:15:02 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int> mp;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+        }
+        int n;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp[nums[i]]>(nums.size()/2))
+            {
+                n=nums[i];
+            }
+        }
+        return n;
+    }
+};
