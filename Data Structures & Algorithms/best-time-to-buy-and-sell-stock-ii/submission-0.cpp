fdea46cd41f6f51310f6e2130f76e1fdd52554be
Raamviyhash <raamviyhash811@gmail.com>
2026-07-28 22:44:58 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        for(int i=0;i<prices.size()-1;i++)
+        {
+            int buy=prices[i];
+            if(prices[i+1]<=prices[i])continue;
+            
+            if(prices[i+1]>prices[i])
+            {
+                int sell=prices[i+1];
+                profit+=sell-buy;                                
+            }
+        }
+        return profit;
+    }
+};
